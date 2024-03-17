bcc9c2c2f26db4373c716d8e4e674feb87d77fc1
prakashtaz0091 <code.ing2468@gmail.com>
2024-03-17 23:58:49 +0545

    sliders js fix

--- card/card.js
+++ card/card.js
@@ -1,21 +1,54 @@
-tailwind.config = {
-    theme: {
-        extend: {
-            fontFamily: {
-                cabinetGrotesk: "'Cabinet Grotesk', san-serif",
-            }
-        }
-    }
-}
+// tailwind.config = {
+//     theme: {
+//         extend: {
+//             fontFamily: {
+//                 cabinetGrotesk: "'Cabinet Grotesk', san-serif",
+//             }
+//         }
+//     }
+// }
+
+//feel alive slider
 const slidesContainer = document.querySelector(".slides-container");
 const slideWidth = slidesContainer.querySelector(".slide").clientWidth;
 const prevButton = document.querySelector(".prev");
 const nextButton = document.querySelector(".next");
 
 nextButton.addEventListener("click", () => {
-    slidesContainer.scrollLeft += slideWidth;
+  console.log("next clicked");
+  slidesContainer.scrollLeft += slideWidth;
 });
 
 prevButton.addEventListener("click", () => {
-    slidesContainer.scrollLeft -= slideWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
+
+//our experts slider
+const slidesContainer2 = document.querySelectorAll(".slides-container")[1];
+const slideWidth2 = slidesContainer.querySelectorAll(".slide")[1].clientWidth;
+const prevButton2 = document.querySelectorAll(".prev")[1];
+const nextButton2 = document.querySelectorAll(".next")[1];
+
+nextButton2.addEventListener("click", () => {
+  console.log("next clicked");
+  slidesContainer2.scrollLeft += slideWidth2;
+});
+
+prevButton2.addEventListener("click", () => {
+  slidesContainer2.scrollLeft -= slideWidth2;
+});
+
+//popular right now slider
+const slidesContainer3 = document.querySelectorAll(".slides-container")[2];
+const slideWidth3 = slidesContainer.querySelectorAll(".slide")[2].clientWidth;
+const prevButton3 = document.querySelectorAll(".prev")[2];
+const nextButton3 = document.querySelectorAll(".next")[2];
+
+nextButton3.addEventListener("click", () => {
+  console.log("next clicked");
+  slidesContainer3.scrollLeft += slideWidth3;
+});
+
+prevButton3.addEventListener("click", () => {
+  slidesContainer3.scrollLeft -= slideWidth3;
 });
